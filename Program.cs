8dacaace4b5c091d63e185485bba97e831a40b75
user <fjodor.la@gmail.com>
2020-09-02 18:45:54 +0300

    My initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyFirstWebApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
